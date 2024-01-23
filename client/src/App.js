e6aa775389e9149de54221e541ff0ef36155e510
Pranav Bali <93444863+XthPB@users.noreply.github.com>
2024-01-23 22:41:20 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+/** import all components */
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+import Verify from './components/verify_otp';
+import Verifyregister from './components/verify_register';
+
+
+/** auth middleware */
+import { AuthorizeUser, ProtectRoute } from './middleware/auth'
+
+/** root routes */
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/password',
+        element : <ProtectRoute><Password /></ProtectRoute>
+    },
+    {
+        path : '/profile',
+        element : <AuthorizeUser><Profile /></AuthorizeUser>
+    },
+    {
+        path : '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path : '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path : '/verify_otp',
+        element : <Verify></Verify>
+    },
+    {
+        path : '/verify_register',
+        element : <Verifyregister></Verifyregister>
+    },
+    {
+        path : '*',
+        element : <PageNotFound></PageNotFound>
+    },
+])
+
+export default function App() {
+  return (
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
+}
+
+
---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "lWjkV8nAIbC7aPzjhqbtOW2oDvr9hTsHYu056v6tOGE=",
+    EMAIL: "eusebio.yost@ethereal.email", // testing email & password
+    PASSWORD : "u6jBn118qT1fMYCBQw",
+    ATLAS_URI: "mongodb+srv://devansha939:DI4GZJwSk7qAaDwe@cluster0.u0r7m27.mongodb.net/?retryWrites=true&w=majority"
+}
